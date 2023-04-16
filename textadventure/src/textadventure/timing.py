0a6c226cfce5e8365cea7572a0474d885c8245eb
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2023-04-17 07:51:27 +1000

    Add files via upload

---
+++ textadventure/src/textadventure/timing.py
@@ -0,0 +1,9 @@
+from time import perf_counter
+from typing import Callable
+
+
+def timeEvent(event: Callable, *params) -> (float, any):
+	start_time = perf_counter()
+	output = event(*params)
+	end_time = perf_counter()
+	return (end_time - start_time, output)
